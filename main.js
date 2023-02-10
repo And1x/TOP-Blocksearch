1e8f4f4c5432e95cb1d411e37e1d03d0e573af30
And1 <92379947+And1x@users.noreply.github.com>
2023-02-10 20:23:57 +0100

    search block added

--- main.js
+++ main.js
@@ -1,59 +1,75 @@
+import {
+  getCurrentBlockHeight,
+  getBlockByHeight,
+  getBlockByHash,
+  getBlockHashByHeigth,
+} from "./services/mempool.space.js";
+
 // html-els
 const containerEl = document.querySelector(".container");
 const blockHeightEl = document.querySelector("#blockHeight");
+const searchEl = document.querySelector("#search");
+const searchBtnEl = document.querySelector("#search-btn");
 
-const hostname = "https://mempool.space";
+// global vars
+let currentBlockHeight;
 
-// fetch it baby
+// validator function
+const isHexadecimal = (str) => /^[A-F0-9]+$/i.test(str);
 
-async function getCurrentBlockHeight() {
-  let url = `${hostname}/api/blocks/tip/height`;
-  try {
-    let res = await fetch(url);
-    return await res.json();
-  } catch (error) {
-    // todo: dont use console.log for err handling
-    console.log(error);
-  }
-}
+searchBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  let searchInput = searchEl.value;
+  // todo: filter if input is blockheight, block-id and evtl. txid
+  searchInputSwitch(searchInput);
 
-async function getBlockByHeight(height) {
-  // 1. get the block hash of the given height
-  let hash = await getBlockHashByHeigth(height);
-  // 2. then get the block infos
-  try {
-    let url = `${hostname}/api/block/${hash}`;
-    let res = await fetch(url);
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+  // if (!isNaN(searchInput) && searchInput <= currentBlockHeight) {
+  //   getBlockByHeight(Number(searchInput)).then(function (res) {
+  //     removeBlockInfoTable();
+  //     containerEl.appendChild(createBlockInfoTable(res));
+  //   });
+  // } else {
+  //   console.log("wrong input yoo");
+  // }
+});
 
-async function getBlockHashByHeigth(height) {
-  let url = `${hostname}/api/block-height/${height}`;
-  try {
-    let res = await fetch(url);
-    return await res.text();
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// could use a async function aswell to await getCurrentBlock in there
-getCurrentBlockHeight().then(function (res) {
-  const blocktimeMsg = `Current Blocktime: ${res}`;
+// ####################
+// app logic block - or init()??
+getCurrentBlockHeight().then(function (height) {
+  const blocktimeMsg = `Current Blocktime: ${height}`;
   blockHeightEl.innerText = blocktimeMsg;
+
+  currentBlockHeight = height;
+
+  getBlockByHeight(height).then(function (res) {
+    containerEl.appendChild(createBlockInfoTable(res));
+  });
 });
 
-getBlockByHeight(775769).then(function (res) {
-  containerEl.appendChild(createBlockInfoTable(res));
-});
+// note: see n1 in README
+function searchInputSwitch(searchInput) {
+  if (isHexadecimal(searchInput) && searchInput.length === 64) {
+    // find block by Hash -> if no result use hash to search TX
+    getBlockByHash(searchInput).then(function (block) {
+      removeBlockInfoTable();
+      containerEl.appendChild(createBlockInfoTable(block));
+    });
+  } else if (
+    Number(searchInput) <= Number(currentBlockHeight) &&
+    Number(searchInput) >= 0
+  ) {
+    getBlockByHeight(Number(searchInput)).then(function (heigth) {
+      removeBlockInfoTable();
+      containerEl.appendChild(createBlockInfoTable(heigth));
+    });
+  }
+}
 
 // ####################
 // create HTML elements
 function createBlockInfoTable(tableData) {
   let table = document.createElement("table");
+  table.setAttribute("id", "blockinfo-table");
   let thead = table.createTHead();
   let tRow = thead.insertRow();
 
@@ -79,3 +95,12 @@ function createBlockInfoTable(tableData) {
 
   return table;
 }
+
+function removeBlockInfoTable() {
+  const table = document.getElementById("blockinfo-table");
+  if (table) {
+    table.remove();
+  }
+}
+
+// npub1752z39hdxwsx9xte0pjr8pmehg3f53mxj09qx7362le9gs8ef5gs6y6uhs
---
+++ services/mempool.space.js
@@ -0,0 +1,41 @@
+const hostname = "https://mempool.space";
+
+// fetch it baby
+
+export async function getCurrentBlockHeight() {
+  let url = `${hostname}/api/blocks/tip/height`;
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    // todo: dont use console.log for err handling
+    console.log(error);
+  }
+}
+
+export async function getBlockByHeight(height) {
+  // 1. get the block hash of the given height
+  let hash = await getBlockHashByHeigth(height);
+  // 2. then get the block infos
+  return await getBlockByHash(hash);
+}
+
+export async function getBlockByHash(hash) {
+  try {
+    let url = `${hostname}/api/block/${hash}`;
+    let res = await fetch(url);
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getBlockHashByHeigth(height) {
+  let url = `${hostname}/api/block-height/${height}`;
+  try {
+    let res = await fetch(url);
+    return await res.text();
+  } catch (error) {
+    console.log(error);
+  }
+}
