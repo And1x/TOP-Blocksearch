8218134531365320fe6384e9c82a08cfd2b4173a
And1 <92379947+And1x@users.noreply.github.com>
2023-02-10 11:59:51 +0100

    basic functionality

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // html-els
-const blockHeight = document.querySelector("#blockHeight");
+const containerEl = document.querySelector(".container");
+const blockHeightEl = document.querySelector("#blockHeight");
+
+const hostname = "https://mempool.space";
 
 // fetch it baby
 
-async function getCurrentBlock() {
-  let url = "https://mempool.space/api/blocks/tip/height";
+async function getCurrentBlockHeight() {
+  let url = `${hostname}/api/blocks/tip/height`;
   try {
     let res = await fetch(url);
     return await res.json();
@@ -14,7 +17,65 @@ async function getCurrentBlock() {
   }
 }
 
+async function getBlockByHeight(height) {
+  // 1. get the block hash of the given height
+  let hash = await getBlockHashByHeigth(height);
+  // 2. then get the block infos
+  try {
+    let url = `${hostname}/api/block/${hash}`;
+    let res = await fetch(url);
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getBlockHashByHeigth(height) {
+  let url = `${hostname}/api/block-height/${height}`;
+  try {
+    let res = await fetch(url);
+    return await res.text();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 // could use a async function aswell to await getCurrentBlock in there
-getCurrentBlock().then(function (res) {
-  blockHeight.innerText = res;
+getCurrentBlockHeight().then(function (res) {
+  const blocktimeMsg = `Current Blocktime: ${res}`;
+  blockHeightEl.innerText = blocktimeMsg;
 });
+
+getBlockByHeight(775769).then(function (res) {
+  containerEl.appendChild(createBlockInfoTable(res));
+});
+
+// ####################
+// create HTML elements
+function createBlockInfoTable(tableData) {
+  let table = document.createElement("table");
+  let thead = table.createTHead();
+  let tRow = thead.insertRow();
+
+  let tDesc = document.createElement("th");
+  tDesc.innerText = "Desc.";
+  let tVal = document.createElement("th");
+  tVal.innerText = "Value";
+
+  tRow.appendChild(tDesc);
+  tRow.appendChild(tVal);
+
+  // tableBody
+  let tbody = document.createElement("tbody");
+
+  for (const [key, value] of Object.entries(tableData)) {
+    let tempRow = tbody.insertRow();
+    let tempCellKey = tempRow.insertCell();
+    let tempCellValue = tempRow.insertCell();
+    tempCellKey.appendChild(document.createTextNode(key));
+    tempCellValue.appendChild(document.createTextNode(value));
+  }
+  table.appendChild(tbody);
+
+  return table;
+}
